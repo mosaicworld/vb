e041ce12c7d7ed24ffbcbd9d80012a64cfa3bacb
mosaic_world <mosaic_world@yahoo.com>
2013-11-18 12:38:51 -0800

    added logic so when 0 doubles players and 0 group players are selected, the empty header for court results no longer displays. also rearranged order of court results from smallest to largest (2's, 3's, 4's).

--- js/vb_main.js
+++ js/vb_main.js
@@ -88,19 +88,21 @@ function numSizeCourts () {
 	}
 
 	//print num courts and team sizes
-	$('#court_results').append('<li><h4>Court Results:</h4></li>');
+	if (NumDoublesPlayers > 0 || NumGroupPlayers > 0) {
+		$('#court_results').append('<li><h4>Court Results:</h4></li>');
+	}
 
-	if (Num4Courts > 0) {
-		$('#court_results').append('<li><p>'+Num4Courts+' court(s) for 4\'s </p></li>');
-		console.log(Num4Courts + " courts for 4's");
+	if (Num2CourtsFixed + Num2CourtsGroup > 0) {
+		$('#court_results').append('<li><p>'+TotNum2Courts+' court(s) for 2\'s </p></li>');
+		console.log(TotNum2Courts + " courts for 2's");
 	}
 	if (Num3Courts > 0) {
 		$('#court_results').append('<li><p>'+Num3Courts+' court(s) for 3\'s </p></li>');
 		console.log(Num3Courts + " courts for 3's");
 	}
-	if (Num2CourtsFixed + Num2CourtsGroup > 0) {
-		$('#court_results').append('<li><p>'+TotNum2Courts+' court(s) for 2\'s </p></li>');
-		console.log(TotNum2Courts + " courts for 2's");
+	if (Num4Courts > 0) {
+		$('#court_results').append('<li><p>'+Num4Courts+' court(s) for 4\'s </p></li>');
+		console.log(Num4Courts + " courts for 4's");
 	}
 	if (ExtraPlayer > 0) {
 		$('#court_results').append('<li><p>'+ExtraMessage+'</p></li>');
